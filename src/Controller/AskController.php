83df0f14cc2ee7fd44e20351251800cf6e210a6f
Buffeteau Maxime <max.buffeteau@gmail.com>
2020-03-26 17:00:31 +1100

    ask controller

---
+++ src/Controller/AskController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AskController extends AbstractController
+{
+    /**
+     * @Route("/les-missions", name="asklisting")
+     */
+    public function index()
+    {
+        return $this->render('ask/vuelisting.html.twig', [
+            'controller_name' => 'AskController',
+        ]);
+    }
+}
